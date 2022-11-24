87eda3799d07498a64b56852f3059fffc61e4a69
David Zamora <davidzs1596@gmail.com>
2022-11-24 08:17:44 -0600

    add: Styles

---
+++ EntidadesModulo.py
@@ -0,0 +1,13 @@
+class Persona:
+    def __init__(self,cedula,nombre,genero,sector,tipoCredito,monto,tasa,plazo):
+        self.cedula = cedula
+        self.nombre = nombre
+        self.sector = sector
+        self.genero = genero
+        self.tipoCredito = tipoCredito
+        self.monto = monto
+        self.tasa = tasa
+        self.plazo = plazo
+        
+
+ 
