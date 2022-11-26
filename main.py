144823a687bc115b9a0bcd8f2f356a8393dab884
Andre Rodriguez <joserox1218@gmail.com>
2022-11-25 20:16:38 -0600

    se regreso a la version anterior y se creo la qtable sin los botones eliminar y modificar

--- main.py
+++ main.py
@@ -46,6 +46,12 @@ class main(QtWidgets.QDialog):
         )
         self.btnGenerar.clicked.connect(self.escribirArchivo)
 
+        # controlador de columnas y set de tablewidget
+        self.fila = 0
+        self.tableWidget = self.findChild(
+            QtWidgets.QTableWidget, "tableWidget"
+        )
+
     def escribirArchivo(self):
         try:
 
@@ -66,6 +72,9 @@ class main(QtWidgets.QDialog):
             archivo = open(nombre, "a")
             archivo.write(infoClienteCredito)
             archivo.close()
+
+            # Escribir en la tabla
+            self.escribirTabla()
             # Seteando contoles a sus valores orignales
             self.inicializarControles()
 
@@ -76,7 +85,32 @@ class main(QtWidgets.QDialog):
             archivo.close()
             print(traceback.format_exc())
 
+    def escribirTabla(self):
+        cedula = QtWidgets.QTableWidgetItem(self.txtCedula.text())
+        nombre = QtWidgets.QTableWidgetItem(self.txtNombre.text())
+        monto = QtWidgets.QTableWidgetItem(self.txtMonto.text())
+        tasa = QtWidgets.QTableWidgetItem(self.txtTasa.text())
+        genero = QtWidgets.QTableWidgetItem(self.comboBoxGenero.currentText())
+        plazo = QtWidgets.QTableWidgetItem(self.comboBoxPlazo.currentText())
+        sector = QtWidgets.QTableWidgetItem(self.comboBoxSector.currentText())
+        tipoCredito = QtWidgets.QTableWidgetItem(
+            self.comboBoxTipoCredito.currentText())
+
+        # generar el texto
+        self.tableWidget.insertRow(self.fila)
+        self.tableWidget.setItem(self.fila, 0, cedula)
+        self.tableWidget.setItem(self.fila, 1, nombre)
+        self.tableWidget.setItem(self.fila, 2, genero)
+        self.tableWidget.setItem(self.fila, 3, sector)
+        self.tableWidget.setItem(self.fila, 4, tipoCredito)
+        self.tableWidget.setItem(self.fila, 5, monto)
+        self.tableWidget.setItem(self.fila, 6, tasa)
+        self.tableWidget.setItem(self.fila, 7, plazo)
+
+        self.fila += 1
+
     def inicializarControles(self):
+
         self.txtCedula.clear()
         self.txtMonto.clear()
         self.txtNombre.clear()
