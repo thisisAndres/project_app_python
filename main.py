bf0fc6b9651a8c80662dbcc5e77a46375553f85c
Andre Rodriguez <joserox1218@gmail.com>
2022-11-25 20:02:52 -0600

    Revert "add: Styles"
    This reverts commit 87eda3799d07498a64b56852f3059fffc61e4a69.

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from PyQt6 import QtWidgets, uic
 import os
 import sys
-from FrmCotizacion import *
-
 import traceback
-from EntidadesModulo import Persona
-from PyQt6 import QtWidgets
 
 
 class main(QtWidgets.QDialog):
@@ -50,126 +46,7 @@ class main(QtWidgets.QDialog):
         )
         self.btnGenerar.clicked.connect(self.escribirArchivo)
 
-class FrmCotizacion(QtWidgets.QDialog):
-        def __init__(self):
-            super(FrmCotizacion,self).__init__()
-            super().__init__()
-            self.ui = Ui_Form()
-            self.objPersona = None
-            self.fila = 0
-            self.ui.setupUi(self)
-            self.inicializarControles()
-            self.ui.BtnGenerar.clicked.connect(self.generarCotizacion)
-            self.ui.BtnEliminar.clicked.connect(self.eliminarCotizacion)
-            self.ui.BtnModificar.clicked.connect(self.modificarCotizacion)
-            self.ui.BtnEliminar.setDisabled(True)
-            self.ui.BtnModificar.setDisabled(True)
-            self.ui.TblInformacion.selectionModel().currentRowChanged.connect(self.onSelectionChanged)
-
-        def generarCotizacion(self):
-            cedula = self.ui.txtCedula.text()
-            nombre = self.ui.txtNombre.text()
-            genero = self.ui.comboBoxGenero.currentText()
-            sector = self.ui.comboBoxSector.currentText()
-            tipoCredito = self.ui.comboBoxTipoCredito.currentText()
-            monto = self.ui.txtMonto.text()
-            tasa = self.ui.txtTasa.text()
-            plazo = self.ui.comboBoxPlazo.currentText()
-
-           
-            self.objPersona = Persona(cedula,nombre,genero,sector,tipoCredito,monto,tasa,plazo)
-            self.ui.TblInformacion.insertRow(self.fila)
-            celdaCedula = QtWidgets.QTableWidgetItem(self.objPersona.cedula)
-            celdaNombre = QtWidgets.QTableWidgetItem(self.objPersona.nombre)
-            celdaGenero = QtWidgets.QTableWidgetItem(self.objPersona.genero)
-            celdaSector = QtWidgets.QTableWidgetItem(self.objPersona.sector)
-            celdaCredito = QtWidgets.QTableWidgetItem(self.objPersona.tipoCredito)
-            celdaMonto = QtWidgets.QTableWidgetItem(self.objPersona.monto)
-            celdaTasa = QtWidgets.QTableWidgetItem(self.objPersona.tasa)
-            celdaPlazo = QtWidgets.QTableWidgetItem(self.objPersona.plazo)
-
-            
-            self.ui.TblInformacion.setItem(self.fila, 0, celdaCedula)
-            self.ui.TblInformacion.setItem(self.fila, 1, celdaNombre)
-            self.ui.TblInformacion.setItem(self.fila, 2, celdaGenero)
-            self.ui.TblInformacion.setItem(self.fila, 3, celdaSector)
-            self.ui.TblInformacion.setItem(self.fila, 4, celdaCredito)
-            self.ui.TblInformacion.setItem(self.fila, 5, celdaMonto)
-            self.ui.TblInformacion.setItem(self.fila, 6, celdaTasa)
-            self.ui.TblInformacion.setItem(self.fila, 7, celdaPlazo)
-
-
-            self.fila += 1
-            self.inicializarControles()
-
-        def eliminarCotizacion(self):
-            indiceAEliminar = self.ui.TblInformacion.currentRow()
-            self.ui.TblInformacion.removeRow(indiceAEliminar)
-            self.fila -= 1
-            self.ui.TblInformacion.selectionModel().clearCurrentIndex()
-
-        def modificarCotizacion(self):
-            indiceAEditar = self.ui.TblInformacion.currentRow()
-            cedula = self.ui.txtCedula.text()
-            nombre = self.ui.txtNombre.text()
-            genero = self.ui.comboBoxGenero.currentText()
-            sector = self.ui.comboBoxSector.currentText()
-            tipoCredito = self.ui.comboBoxTipoCredito.currentText()
-            monto = self.ui.txtMonto.text()
-            tasa = self.ui.txtTasa.text()
-            plazo = self.ui.comboBoxPlazo.currentText()
-            
-            genero = self.ui.comboBoxGenero.currentText()
-            self.objPersona = Persona(cedula,nombre,genero,sector,tipoCredito,monto,tasa,plazo)
-            celdaCedula = QtWidgets.QTableWidgetItem(self.objPersona.cedula)
-            celdaNombre = QtWidgets.QTableWidgetItem(self.objPersona.nombre)
-            celdaGenero = QtWidgets.QTableWidgetItem(self.objPersona.genero)
-            celdaSector = QtWidgets.QTableWidgetItem(self.objPersona.sector)
-            celdaCredito = QtWidgets.QTableWidgetItem(self.objPersona.tipoCredito)
-            celdaMonto = QtWidgets.QTableWidgetItem(self.objPersona.monto)
-            celdaTasa = QtWidgets.QTableWidgetItem(self.objPersona.tasa)
-            celdaPlazo = QtWidgets.QTableWidgetItem(self.objPersona.plazo)
-
-            
-            self.ui.TblInformacion.setItem(indiceAEditar,0,celdaCedula)
-            self.ui.TblInformacion.setItem(indiceAEditar,1,celdaNombre)
-            self.ui.TblInformacion.setItem(indiceAEditar,2,celdaGenero)
-            self.ui.TblInformacion.setItem(indiceAEditar,3,celdaSector)
-            self.ui.TblInformacion.setItem(indiceAEditar,4,celdaCredito)
-            self.ui.TblInformacion.setItem(indiceAEditar,5,celdaMonto)
-            self.ui.TblInformacion.setItem(indiceAEditar,6,celdaTasa)
-            self.ui.TblInformacion.setItem(indiceAEditar,7,celdaPlazo)
-
-            self.inicializarControles()
-            self.ui.TblInformacion.selectionModel().clearCurrentIndex()      
-
-
-        def onSelectionChanged(self, selected):
-            if (selected.row() != -1):
-                self.ui.BtnEliminar.setEnabled(True)
-                self.ui.BtnModificar.setEnabled(True)
-            else:
-                self.ui.BtnEliminar.setDisabled(True)
-                self.ui.BtnModificar.setDisabled(True)
-
-        def inicializarControles(self):
-            self.ui.txtCedula.clear()
-            self.ui.txtNombre.clear()
-            self.ui.txtMonto.clear()
-            self.ui.txtTasa.clear()
-
-            self.ui.comboBoxGenero.setCurrentIndex(0)
-            self.ui.comboBoxSector.setCurrentIndex(2)
-            self.ui.comboBoxTipoCredito.setCurrentIndex(2)
-            self.ui.comboBoxPlazo.setCurrentIndex(2)
-
-if __name__ == "__main__":
-        app = QtWidgets.QApplication(sys.argv)
-        myapp = FrmCotizacion()
-        myapp.show()
-        sys.exit(app.exec())
-
-def escribirArchivo(self):
+    def escribirArchivo(self):
         try:
 
             infoClienteCredito = (
@@ -199,7 +76,7 @@ def escribirArchivo(self):
             archivo.close()
             print(traceback.format_exc())
 
-def inicializarControles(self):
+    def inicializarControles(self):
         self.txtCedula.clear()
         self.txtMonto.clear()
         self.txtNombre.clear()
